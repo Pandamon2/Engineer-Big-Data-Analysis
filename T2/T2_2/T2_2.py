245358ef106baf7d9acbf1efab4d52f644d40812
hajun <hajun2317@gmail.com>
2023-06-09 17:25:45 +0900

    230609 17:00 수정

---
+++ T2/T2_2/T2_2.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+def exam_data_load(df, target, id_name="", null_name=""):
+    if id_name == "":
+        df = df.reset_index().rename(columns={"index": "id"})
+        id_name = 'id'
+    else:
+        id_name = id_name
+
+    if null_name != "":
+        df[df == null_name] = np.nan
+
+    X_train, X_test = train_test_split(df, test_size=0.2, random_state=2021)
+
+    y_train = X_train[[id_name, target]]
+    X_train = X_train.drop(columns=[target])
+
+    y_test = X_test[[id_name, target]]
+    X_test = X_test.drop(columns=[target])
+    return X_train, X_test, y_train, y_test
+
+df = pd.read_csv("D:/실습/T2/T2_2/diabetes.csv")
+
+# 데이터 확인
+df.info()
+df.isnull().sum()
+df.shape
+df.head()
+
